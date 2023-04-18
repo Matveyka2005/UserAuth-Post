0a0d50afff8ea406a0926c2318f13bc81e543211
Matvey <matveykawork@gmail.com>
2023-04-18 16:53:37 +0300

    added groups, ban users and some other things

--- website/main/views.py
+++ website/main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User, Group
 
 from .forms import RegisterForm, PostForm
 from .models import Post
@@ -16,16 +17,33 @@ def home(request):
     
     if request.method == 'POST':
         post_id = request.POST.get("post-id")
+        user_id = request.POST.get("user-id")
+        
         if post_id:
-            post = Post.objects.get(pk=post_id).select_related('author')
-            if post and post.author == request.user:
+            post = Post.objects.select_related('author').get(pk=post_id)
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
                 post.delete()
-            
+        elif user_id:
+            user = User.objects.filter(pk=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='default')
+                    group.user_set.remove(user)
+                except Exception as e:
+                    pass
+                
+                try:
+                    group = Group.objects.get(name='mod')
+                    group.user_set.remove(user)
+                except Exception as e:
+                    pass 
     return render(request, 'main/home.html', {'posts': posts})
 
 
 
 @login_required(login_url=reverse_lazy('login'))
+@permission_required("main.add_post", login_url=reverse_lazy('login'),
+                     raise_exception=True)
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
