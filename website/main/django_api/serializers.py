d3ad17b13a89676bbf240d2c09a1e47f8dd57f6f
Matvey <matveykawork@gmail.com>
2023-04-18 18:56:01 +0300

    Revert "permissions"
    This reverts commit b65bdb3ba1d80a82a98c1fedb0bab74015cae796.

--- website/main/django_api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-
-from ..models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ("author", "title", "description", "created_at", "updated_at")
